5a191c0b72eae91284c4a86b9c7ef0e4145daf91
Sudarshan Lawati <sudarshanlimboo68@gmail.com>
2024-01-10 20:57:25 +0530

    Added comments to singleProduct.js file

--- js/singleProduct.js
+++ js/singleProduct.js
@@ -1,16 +1,22 @@
+// Importing singleProduct function from ./functions.js file
 import { singleProduct } from "./functions.js";
 
+// An event listener listening to the loading of DOM.
+// when event is triggered, an anonymous async function runs.
 document.addEventListener("DOMContentLoaded", async function () {
 	console.log("Welcome to single product page");
 
+	// Selecting displayArea from Html Page.
 	const displayArea = document.querySelector(".product");
 
-	// Accessing query parameters
+	// Accessing query parameters from URL.
 	const searchParams = new URLSearchParams(window.location.search);
 	const id = searchParams.get("id");
 
+	// Fetching a single product.
 	const product = await singleProduct(id);
 
+	// Rendering a product information in Html page.
 	const productContainer = document.createElement("div");
 
 	const productImage = document.createElement("img");
